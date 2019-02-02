f8040c13c4307b7f2db318f041aa648fb0be28b4
unknown <hahajeir@gmail.com>
2019-02-02 12:24:12 +0300

    first commit

---
+++ 4-LM35_Temperature/4-LM35_Temperature.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(D1,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int analogValue = analogRead(A0);
+  float analogVoltage = analogValue * 3.3 / 1023 ;
+  float temperature = analogVoltage / 0.01 ;
+  
+  Serial.println(temperature);
+
+  if(temperature > 25){
+    digitalWrite(D1,HIGH);
+  }
+  else {
+    digitalWrite(D1,LOW);
+  }
+}
