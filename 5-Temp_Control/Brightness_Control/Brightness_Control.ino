e68aa41662a358f765712f27950e153fb3019c5c
unknown <hahajeir@gmail.com>
2019-02-27 19:53:23 +0300

    5th Video's files

---
+++ 5-Temp_Control/Brightness_Control/Brightness_Control.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(D1,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int analogValue = analogRead(A0);
+  float analogVoltage = analogValue * 3.3 / 1023 ;
+  float temperature = analogVoltage / 0.01 ;
+  
+  Serial.println(temperature);
+
+  int rate =  map(temperature , 28 , 50 , 0 , 1023 );
+  
+  analogWrite(D1,rate);
+  
+}
